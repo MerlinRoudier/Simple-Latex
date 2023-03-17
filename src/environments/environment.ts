c82ae2afa8118fc9177be6f8916620c9efc52445
github_username <wyzmatt@gmail.com>
2023-03-17 09:23:42 +0100

    ajout de materials

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment= {
+  firebase: {
+    projectId: 'simple-latex',
+    appId: '1:748449464383:web:19402825bd1403fcadf88a',
+    storageBucket: 'simple-latex.appspot.com',
+    apiKey: 'AIzaSyBb90XU5wWgvudyk7XJE0qqzOuX7UcbvWI',
+    authDomain: 'simple-latex.firebaseapp.com',
+    messagingSenderId: '748449464383',
+  },production: false};
