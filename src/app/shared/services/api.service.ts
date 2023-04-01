243116799653c8f8f94ca89b102c3f00881c6908
Enyobii <stalobize@gmail.com>
2023-04-01 15:06:14 +0200

    toLaTeXUrl improved

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -18,6 +18,41 @@ export class ApiService {
     let latexEquation = input;
     latexEquation=latexEquation.replace(/(\w+)\/(\w+)/g, '\\frac{$1}{$2}');
     latexEquation=latexEquation.replace(/pi/g, '\\pi');
+    latexEquation=latexEquation.replace(/alpha/g, '\\alpha');
+    latexEquation=latexEquation.replace(/beta/g, '\\beta');
+    latexEquation=latexEquation.replace(/gamma/g, '\\gamma');
+    latexEquation=latexEquation.replace(/epsilon/g, '\\epsilon');
+    latexEquation = latexEquation.replace(/delta/g, '\\delta');
+    latexEquation = latexEquation.replace(/theta/g, '\\theta');
+    latexEquation = latexEquation.replace(/lambda/g, '\\lambda');
+    latexEquation = latexEquation.replace(/mu/g, '\\mu');
+    latexEquation = latexEquation.replace(/\*/g, '\\times');
+    const mathFunctions = ['cos', 'sin', 'tan', 'log', 'max', 'min','sum', 'int'];
+    mathFunctions.forEach(func => {
+        const regex = new RegExp(`\\b${func}\\b`, 'g');
+        latexEquation = latexEquation.replace(regex, `\\${func}`);
+    });
+    const arrows = {
+      '->': '\\rightarrow',
+      '<-': '\\leftarrow',
+      '<->': '\\leftrightarrow'
+  };
+  Object.entries(arrows).forEach(([arrow, replacement]) => {
+      const regex = new RegExp(arrow.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
+      latexEquation = latexEquation.replace(regex, replacement);
+  });
+    latexEquation = latexEquation.replace(/\*\*/g, '^');
+    const comparisonOperators = {
+      '<=': '\\leq',
+      '>=': '\\geq',
+      '!=': '\\neq',
+      '<': '<',
+      '>': '>'
+  };
+  Object.entries(comparisonOperators).forEach(([operator, replacement]) => {
+    const regex = new RegExp(operator.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
+    latexEquation = latexEquation.replace(regex, replacement);
+    });
     //const output = encodeURIComponent(latexEquation);
     const output = latexEquation;
     return `${output}`;
